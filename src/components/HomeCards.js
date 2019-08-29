fe6b5ef8f20c9a0f7adce7e1c5e055edc9031096
Ryan Johnson <ryan@peaktwo.com>
2019-08-29 12:17:34 -0400

    Reflowing state a bit to control cards better

--- src/components/HomeCards.js
+++ src/components/HomeCards.js
@@ -1,9 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
-
 class HomeCards extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      singleOpen: false,
+    };
+
+    this.toggleSingle = this.toggleSingle.bind(this);
+    this.handler = this.handler.bind(this);
+  }
+
+  toggleSingle() {
+    this.setState({
+      singleOpen: !this.state.singleOpen
+    });
+  }
+
+  handler(){
+    this.setState({
+      singleOpen: !this.state.singleOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className={`c_homeCards single-open-${this.state.singleOpen}`}>
+        <div className="homeCards_wrapper is-clearfix">
+          {this.props.cards.map(card => (
+            <HomeCard 
+              title={card.solution}
+              image={card.solutionImage.childImageSharp.fluid.src}
+              quote={card.solutionQuote}
+              slides={card.solutionInfo}
+              action={this.handler}
+            />
+          ))}     
+        </div>
+      </div>
+    )
+  }
+}
+
+HomeCards.propTypes = {
+  cards: PropTypes.array,
+}
+
+// INDIVIDUAL CARD COMPONENT (Main Piece Here)
+class HomeCard extends React.Component {
   constructor(){
     super();
 
@@ -12,7 +58,6 @@ class HomeCards extends React.Component {
     };
 
     this.toggleSingleCard = this.toggleSingleCard.bind(this);
-
   }
 
   cardImageStyle = (src) => ({
@@ -20,52 +65,43 @@ class HomeCards extends React.Component {
   });
 
   toggleSingleCard() {
-    const currentState = this.state.cardOpen;
-    this.setState({ cardOpen: !currentState });
-};
+    this.setState({
+      cardOpen: !this.state.cardOpen
+    });
+    this.props.action();
+  };
 
   render() {
     return (
-      <div className={`c_homeCards single-open-${this.state.cardOpen}`}>
-        <div className="homeCards_wrapper is-clearfix">
-          {this.props.cards.map(card => (
-            <div key={card.solution} className={`homeCards_card ${this.state.cardOpen ? 'card-open': null} `}>
-              <div>                    
-                <figure 
-                  className="card_image" 
-                  style={this.cardImageStyle(card.solutionImage.childImageSharp.fluid.src)}
-                >
-                  <h3 className="title is-size-4 is-uppercase has-text-weight-bold">{card.solution}</h3>
-                </figure>
-                
-                <div className="card_content">
-                  <p>{card.solutionQuote}</p>
-                  <button 
-                    onClick={() => {
-                      this.toggleSingleCard();
-                      this.setState({cardOpen: !this.state.cardOpen});
-                    }}
-                    data-solution={card.solution}
-                    data-image={card.solutionImage.childImageSharp.fluid.src}
-                    data-quote={card.solutionQuote}
-                    data-slides={JSON.stringify(card.solutionInfo)}
-                    className="button is-primary is-thin"
-                  >
-                    Explore More
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}     
+      <div key={this.props.title} className={`homeCards_card ${this.state.cardOpen ? 'card-open': ''} `}>
+        <div>                    
+          <figure 
+            className="card_image" 
+            style={this.cardImageStyle(this.props.image)}
+          >
+            <h3 className="title is-size-4 is-uppercase has-text-weight-bold">{this.props.title}</h3>
+          </figure>
+          
+          <div className="card_content">
+            <p>{this.props.quote}</p>
+            <button 
+              onClick={this.toggleSingleCard}
+              className="button is-primary is-thin"
+            >
+              Explore More
+            </button>
+          </div>
         </div>
-        
       </div>
     )
   }
 }
 
-HomeCards.propTypes = {
-  cards: PropTypes.array,
+HomeCard.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.array,
+  quote: PropTypes.string,
+  slides: PropTypes.array,
 }
 
 export default HomeCards
